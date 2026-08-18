fe1f59d01d18f6c60d2cbf0ded89f12eb9c278b5
lukasffz <lukasffaza@gmail.com>
2026-08-18 15:39:24 -0300

    feat(backend): initial setup with spring boot, postgresql, flyway and dotenv

---
+++ backend-taskmaster/src/main/java/com/taskmaster/TaskMasterApplication.java
@@ -0,0 +1,28 @@
+package com.taskmaster;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMasterApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskMasterApplication.class);
+
+    public static void main(String[] args) {
+        try {
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+            dotenv.entries().forEach(entry -> {
+                if (System.getProperty(entry.getKey()) == null && System.getenv(entry.getKey()) == null) {
+                    System.setProperty(entry.getKey(), entry.getValue());
+                }
+            });
+        } catch (Exception e) {
+            logger.warn("Local environment configuration not found, using system variables.");
+        }
+
+        SpringApplication.run(TaskMasterApplication.class, args);
+    }
+}
